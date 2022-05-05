484634b2e94e93526e36c3a7f8257b88192cf656
Tamilarasan S <tarasan833@gmail.com>
2022-05-05 17:43:22 +0530

    first commit

---
+++ src/main/java/com/bankapp/BankappUserMsApiApplication.java
@@ -0,0 +1,14 @@
+package com.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BankappUserMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankappUserMsApiApplication.class, args);
+	}
+
+}
